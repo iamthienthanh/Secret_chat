c19bc46ccb0a1e7b07570f2b6b04be53d69e28e4
Thiện Thành Nguyễn <109054326+tian-nguyen@users.noreply.github.com>
2022-08-25 22:55:17 +0700

    done auth

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { keepLoginRoute } from '../utils/APIRoutes';
+import axios from 'axios';
 const Chat = () => {
-  return (
+    const navigate = useNavigate()
+    useEffect(() => {
+        const keepLogin = async () => {
+            let user_info = JSON.parse(localStorage.getItem('user'));
+            const username = user_info.username;
+            const password = user_info.password;
+            const {data} = await axios.post(keepLoginRoute, {
+                username, password
+            })
+            if (data.success === false) {
+                navigate('/login')
+            }
+        }
+        keepLogin()
+    },[])
+    return (
     <div>Chat</div>
   )
 }
